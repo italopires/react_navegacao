d2cb7d2768b06324196fbdb9f97b4dd5d5f02458
Italo Pires <italonapalm@gmail.com>
2022-07-30 21:11:55 -0300

    feat(params): catching params from url

--- src/components/layouts/Content.jsx
+++ src/components/layouts/Content.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import About from '../../views/examples/About'
 import Home from '../../views/examples/Home'
+import Param from '../../views/examples/Param'
 
 const Content = props => (
   <main className="Content">
     <Routes>
       <Route path="/about" element={<About />} />
-      {/* <Route path="/params/:id" element={<Param />} /> */}
+      <Route path="/param/:id" element={<Param />} />
       <Route path="/" exact element={<Home />} />
       {/* <Route path="*" element={<NotFound />} /> */}
     </Routes>
