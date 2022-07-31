27387c58e77d974a3d8db77b0dfd2ce1df3e2cee
Italo Pires <italonapalm@gmail.com>
2022-07-30 21:20:38 -0300

    feat(notfound): add not found page

--- src/components/layouts/Content.jsx
+++ src/components/layouts/Content.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom'
 import About from '../../views/examples/About'
 import Home from '../../views/examples/Home'
 import Param from '../../views/examples/Param'
+import NotFound from '../../views/examples/NotFound'
 
 const Content = props => (
   <main className="Content">
@@ -11,7 +12,7 @@ const Content = props => (
       <Route path="/about" element={<About />} />
       <Route path="/param/:id" element={<Param />} />
       <Route path="/" exact element={<Home />} />
-      {/* <Route path="*" element={<NotFound />} /> */}
+      <Route path="*" element={<NotFound />} />
     </Routes>
   </main>
 )
